a4b16657a6884a2705afc7cf0a2d98c509a993f7
Kotorkovsciy <kotorkovsciy@gmail.com>
2022-07-31 23:43:43 +0300

    nano update

--- plugins/arduino.py
+++ plugins/arduino.py
@@ -5,7 +5,11 @@ board = Arduino('COM3')
 
 def on_light():
     board.digital[8].write(1)
+    board.digital[2].write(1)
+    board.digital[3].write(1)
 
 
 def off_light():
     board.digital[8].write(0)
+    board.digital[2].write(0)
+    board.digital[3].write(0)
